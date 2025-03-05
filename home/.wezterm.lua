3f96a33275817f575edd4676c3311541dcedbe99
Daniel Sakuma <dsakuma@gmail.com>
2025-03-05 14:04:01 -0300

    Set wezterm theme

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -2,9 +2,7 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'One Dark (Gogh)'
-if wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
-  config.font = wezterm.font 'MesloLGS Nerd Font Mono'
-end
+config.color_scheme = 'OneDark (base16)'
+config.font = wezterm.font 'MesloLGS Nerd Font Mono'
 
 return config
